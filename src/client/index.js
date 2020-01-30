e1f36feedee4072b62ef22e79177641ba3ea8948
aleregue89 <aleregue89@gmail.com>
2020-01-30 00:19:18 -0500

    babel loaders

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,7 @@
 import {handleSubmit} from './js/formHandler';
 import {checkForName} from './js/nameChecker';
 
+console.log(checkForName);
+
 // creating a new file called index.js into the client directory //
 alert('I exist!');
